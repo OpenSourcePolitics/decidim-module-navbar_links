cb1ca11c734c82b10a30d5682d50ac307973a06e
Alexandru Emil Lupu <contact@alecslupu.ro>
2021-02-16 12:48:20 +0200

    Allow menu items to be ordered by weight (#6)

--- app/models/decidim/navbar_links/navbar_link.rb
+++ app/models/decidim/navbar_links/navbar_link.rb
@@ -5,6 +5,8 @@ module Decidim
     class NavbarLink < ApplicationRecord
       belongs_to :organization, foreign_key: "decidim_organization_id", class_name: "Decidim::Organization"
       scope :organization, ->(organization) { where(organization: organization) }
+      scope :ordered, -> { order(:weight)}
+
       before_save :validate_link_regex
 
       def validate_link_regex
